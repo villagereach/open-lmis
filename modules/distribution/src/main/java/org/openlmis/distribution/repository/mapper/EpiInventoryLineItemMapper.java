8dd181919755479d72c8ed8a15df00b7e25b5fe9
Ben Leibert <ben.leibert@villagereach.org>
2016-05-18 11:28:38 -0700

    SIIL-11: EPI Inventory Screen: replace "vials" with "doses"
    Although the relevant values are surely used within reports, OpenLMIS as an application doesn’t otherwise use nor process these values. Changing them from vials to doses therefore has semantic meaning, but no actual ramifications for the code. This check-in thus exclusively entails:
    * Label (semantic) changes.
    * Custom Data validation.
    * The addition of a packSize column to epi_inventory_line_items (see the comment within "V146__add_packsize_to_epi_inventory_line_items.sql" for details).

--- modules/distribution/src/main/java/org/openlmis/distribution/repository/mapper/EpiInventoryLineItemMapper.java
+++ modules/distribution/src/main/java/org/openlmis/distribution/repository/mapper/EpiInventoryLineItemMapper.java
@@ -26,15 +26,15 @@ import java.util.List;
 @Repository
 public interface EpiInventoryLineItemMapper {
 
-  @Insert({"INSERT INTO epi_inventory_line_items (facilityVisitId, programProductId, productCode, productName, productDisplayOrder, idealQuantity, idealQuantityByPackSize, createdBy, modifiedBy) VALUES ",
-    "(#{facilityVisitId}, #{programProductId}, #{productCode}, #{productName}, #{productDisplayOrder}, #{idealQuantity}, #{idealQuantityByPackSize}, #{createdBy}, #{modifiedBy})"})
+  @Insert({"INSERT INTO epi_inventory_line_items (facilityVisitId, programProductId, productCode, productName, productDisplayOrder, idealQuantity, idealQuantityByPackSize, createdBy, modifiedBy, packSize) VALUES ",
+    "(#{facilityVisitId}, #{programProductId}, #{productCode}, #{productName}, #{productDisplayOrder}, #{idealQuantity}, #{idealQuantityByPackSize}, #{createdBy}, #{modifiedBy}, #{productPackSize} )"})
   @Options(useGeneratedKeys = true)
   void insertLineItem(EpiInventoryLineItem lineItem);
 
-  @Select({"SELECT * FROM epi_inventory_line_items WHERE facilityVisitId = #{facilityVisitId} ORDER BY productDisplayOrder, LOWER(productCode)"})
+  @Select({"SELECT eili.*, products.packsize AS productPackSize FROM epi_inventory_line_items eili LEFT JOIN products ON(eili.productcode = products.code) WHERE facilityVisitId = #{facilityVisitId} ORDER BY productDisplayOrder, LOWER(productCode)"})
   List<EpiInventoryLineItem> getLineItemsBy(Long facilityVisitId);
 
   @Update({"UPDATE epi_inventory_line_items SET spoiledQuantity = #{spoiledQuantity}, deliveredQuantity = #{deliveredQuantity},",
-    "existingQuantity = #{existingQuantity}, modifiedBy = #{modifiedBy}, modifiedDate = DEFAULT WHERE id = #{id}"})
+    "existingQuantity = #{existingQuantity}, packSize = #{productPackSize}, modifiedBy = #{modifiedBy}, modifiedDate = DEFAULT WHERE id = #{id}"})
   void updateLineItem(EpiInventoryLineItem lineItem);
 }
