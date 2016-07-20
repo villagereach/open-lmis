ba0d8363752e3c1993bb268f8417f83fbd3ae1a4
Chongsun Ahn <cahn922@yahoo.com>
2016-07-20 14:27:51 -0700

    Add reporting role to database
    Add role to database with select access to certain tables.

---
+++ modules/db/src/main/resources/db/migration/V159__add_olreporter_role.sql
@@ -0,0 +1,15 @@
+CREATE ROLE reporting_role;
+ 
+GRANT SELECT ON TABLE facility_visits_report TO reporting_role;
+GRANT SELECT ON TABLE facility_visits TO reporting_role;
+GRANT SELECT ON TABLE facilities TO reporting_role;
+GRANT SELECT ON TABLE geographic_zones TO reporting_role;
+GRANT SELECT ON TABLE delivery_zones TO reporting_role;
+GRANT SELECT ON TABLE processing_periods TO reporting_role;
+GRANT SELECT ON TABLE distributions TO reporting_role;
+GRANT SELECT ON TABLE refrigerator_problems TO reporting_role;
+GRANT SELECT ON TABLE refrigerator_readings TO reporting_role;
+/*GRANT SELECT ON TABLE vrmis_refrig_readings TO reporting_role;*/
+ 
+CREATE USER olreporter with password 'changeme';
+GRANT reporting_role TO olreporter;
