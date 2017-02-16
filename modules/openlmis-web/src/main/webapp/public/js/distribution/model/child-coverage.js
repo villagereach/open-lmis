c02844d4a12832fc8da5053538a5adecfc98d891
MikolajBasSoldevelo <mbas@soldevelo.com>
2017-02-16 11:37:01 +0100

    Male female fields are not visible for outdated distributions

--- modules/openlmis-web/src/main/webapp/public/js/distribution/model/child-coverage.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/model/child-coverage.js
@@ -22,81 +22,123 @@ function ChildCoverage(facilityVisitId, childCoverageJSON) {
   $(this.openedVialLineItems).each(function (index, lineItem) {
     lineItem.openedVial = lineItem.openedVial || {};
   });
-}
 
-ChildCoverage.prototype.computeStatus = function (visited, review, ignoreSyncStatus) {
-  if (review && !ignoreSyncStatus) {
-    return DistributionStatus.SYNCED;
+  this.isOutdatedDistribution = checkIfIsOutdatedDistribution.call(this);
+  this.mandatoryFields = getMandatoryFields.call(this);
+
+  function getMandatoryFields() {
+    if(this.isOutdatedDistribution) {
+      return ['healthCenter11Months', 'outreach11Months', 'healthCenter23Months', 'outreach23Months'];
+    } else {
+      return ['maleHealthCenter11Months', 'femaleHealthCenter11Months',
+         'maleOutreach11Months', 'femaleOutreach11Months', 'maleHealthCenter23Months', 'femaleHealthCenter23Months',
+         'maleOutreach23Months', 'femaleOutreach23Months'];
+    }
   }
 
-  var status;
+  function isEmpty(obj) {
+    return (isUndefined(obj) || (isUndefined(obj.value) && !obj.notRecorded));
+  }
 
-  var isValid = function (field) {
-    if (!field)
-      return false;
-    return !(isUndefined(field.value) && !field.notRecorded);
+  function maleFemaleFieldsNotPresent(maleField, femaleField, totalField) {
+    return (isEmpty(maleField) && isEmpty(femaleField) && !isEmpty(totalField));
+  }
+
+  function checkIfIsOutdatedDistribution() {
+    outdated = true;
+    $(this.childCoverageLineItems).each(function (index, lineItem) {
+      if(!maleFemaleFieldsNotPresent(lineItem.maleHealthCenter11Months, lineItem.femaleHealthCenter11Months,
+          lineItem.healthCenter11Months)) {
+        outdated = false;
+        return;
+      }
+      if(!maleFemaleFieldsNotPresent(lineItem.maleHealthCenter23Months, lineItem.femaleHealthCenter23Months,
+          lineItem.healthCenter23Months)) {
+        outdated = false;
+        return;
+      }
+      if(!maleFemaleFieldsNotPresent(lineItem.maleOutreach11Months, lineItem.femaleOutreach11Months,
+          lineItem.outreach11Months)) {
+        outdated = false;
+        return;
+      }
+      if(!maleFemaleFieldsNotPresent(lineItem.maleOutreach23Months, lineItem.femaleOutreach23Months,
+          lineItem.outreach23Months)) {
+        outdated = false;
+        return;
+      }
+    });
+    return outdated;
+  }
+
+  ChildCoverage.prototype.computeStatus = function (visited, review, ignoreSyncStatus) {
+    if (review && !ignoreSyncStatus) {
+      return DistributionStatus.SYNCED;
+    }
+
+    var status;
+
+    var isValid = function (field) {
+      if (!field)
+        return false;
+      return !(isUndefined(field.value) && !field.notRecorded);
+    };
+
+    function validateLineItems(lineItems, mandatoryFields) {
+      $(lineItems).each(function (index, lineItem) {
+        $(mandatoryFields).each(function (index, field) {
+          if (lineItem.vaccination === 'Polio (Newborn)' &&
+              mandatoryFields.slice(mandatoryFields.length / 2, mandatoryFields.length).indexOf(field) !== -1)
+            return true;
+          if (lineItem.vaccination === 'IPV' &&
+              mandatoryFields.slice(mandatoryFields.length / 2, mandatoryFields.length).indexOf(field) !== -1)
+            return true;
+          if (lineItem.vaccination === 'Sarampo 2a dose' &&
+              mandatoryFields.slice(0, mandatoryFields.length / 2).indexOf(field) !== -1)
+            return true;
+          if ((status === DistributionStatus.COMPLETE || !status) && isValid(lineItem[field])) {
+            status = DistributionStatus.COMPLETE;
+            return true;
+          } else if ((status === DistributionStatus.EMPTY || !status) && !isValid(lineItem[field])) {
+            status = DistributionStatus.EMPTY;
+            return true;
+          } else if ((status === DistributionStatus.EMPTY && isValid(lineItem[field])) || (status === DistributionStatus.COMPLETE && !isValid(lineItem[field]))) {
+            status = DistributionStatus.INCOMPLETE;
+            return false;
+          }
+          return true;
+        });
+        return status !== DistributionStatus.INCOMPLETE;
+      });
+    }
+
+    validateLineItems(this.childCoverageLineItems, this.mandatoryFields);
+    validateLineItems(this.openedVialLineItems, ['openedVial']);
+
+    this.status = status;
+    return this.status;
   };
 
-  function validateLineItems(lineItems, mandatoryFields) {
-    $(lineItems).each(function (index, lineItem) {
-      $(mandatoryFields).each(function (index, field) {
-        if (lineItem.vaccination === 'Polio (Newborn)' && ['maleHealthCenter23Months', 'femaleHealthCenter23Months',
-            'maleOutreach23Months', 'femaleOutreach23Months'].indexOf(field) !== -1)
-          return true;
-        if (lineItem.vaccination === 'IPV' && ['maleHealthCenter23Months', 'femaleHealthCenter23Months',
-            'maleOutreach23Months', 'femaleOutreach23Months'].indexOf(field) !== -1)
-          return true;
-        if (lineItem.vaccination === 'Sarampo 2a dose' && ['maleHealthCenter11Months', 'femaleHealthCenter11Months',
-            'maleOutreach11Months', 'femaleOutreach11Months'].indexOf(field) !== -1)
-          return true;
-        if ((status === DistributionStatus.COMPLETE || !status) && isValid(lineItem[field])) {
-          status = DistributionStatus.COMPLETE;
-          return true;
-        } else if ((status === DistributionStatus.EMPTY || !status) && !isValid(lineItem[field])) {
-          status = DistributionStatus.EMPTY;
-          return true;
-        } else if ((status === DistributionStatus.EMPTY && isValid(lineItem[field])) || (status === DistributionStatus.COMPLETE && !isValid(lineItem[field]))) {
-          status = DistributionStatus.INCOMPLETE;
-          return false;
-        }
-        return true;
+  function setNotRecorded(field) {
+    if (field) {
+      delete field.value;
+      field.notRecorded = true;
+
+      return field;
+    } else {
+      return {notRecorded: true};
+    }
+  }
+
+  ChildCoverage.prototype.setNotRecorded = function () {
+    fields = this.mandatoryFields;
+    this.childCoverageLineItems.forEach(function (lineItem) {
+      $(fields).each(function (index, field) {
+        lineItem[field] = setNotRecorded(lineItem[field]);
       });
-      return status !== DistributionStatus.INCOMPLETE;
     });
-  }
-
-  validateLineItems(this.childCoverageLineItems, ['maleHealthCenter11Months', 'femaleHealthCenter11Months',
-    'maleOutreach11Months', 'femaleOutreach11Months', 'maleHealthCenter23Months', 'femaleHealthCenter23Months',
-    'maleOutreach23Months', 'femaleOutreach23Months']);
-  validateLineItems(this.openedVialLineItems, ['openedVial']);
-
-  this.status = status;
-  return this.status;
-};
-
-function setNotRecorded(field) {
-  if (field) {
-    delete field.value;
-    field.notRecorded = true;
-
-    return field;
-  } else {
-    return {notRecorded: true};
-  }
+    this.openedVialLineItems.forEach(function (lineItem) {
+      lineItem.openedVial = setNotRecorded(lineItem.openedVial);
+    });
+  };
 }
-
-ChildCoverage.prototype.setNotRecorded = function () {
-  this.childCoverageLineItems.forEach(function (lineItem) {
-    lineItem.maleHealthCenter11Months = setNotRecorded(lineItem.maleHealthCenter11Months);
-    lineItem.maleHealthCenter23Months = setNotRecorded(lineItem.maleHealthCenter23Months);
-    lineItem.maleOutreach11Months = setNotRecorded(lineItem.maleOutreach11Months);
-    lineItem.maleOutreach23Months = setNotRecorded(lineItem.maleOutreach23Months);
-    lineItem.femaleHealthCenter11Months = setNotRecorded(lineItem.femaleHealthCenter11Months);
-    lineItem.femaleHealthCenter23Months = setNotRecorded(lineItem.femaleHealthCenter23Months);
-    lineItem.femaleOutreach11Months = setNotRecorded(lineItem.femaleOutreach11Months);
-    lineItem.femaleOutreach23Months = setNotRecorded(lineItem.femaleOutreach23Months);
-  });
-  this.openedVialLineItems.forEach(function (lineItem) {
-    lineItem.openedVial = setNotRecorded(lineItem.openedVial);
-  });
-};
