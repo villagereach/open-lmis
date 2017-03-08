681e874bc361a54a2e1c22ec9d737a7bf93767de
Sebastian Kostuch <skostuch@soldevelo.com>
2017-03-08 10:53:50 +0100

    Moved incomplete distribution data check so that confirmation dialog always shows up

--- modules/openlmis-web/src/main/webapp/public/js/distribution/controller/record-facility-data-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/controller/record-facility-data-controller.js
@@ -183,6 +183,15 @@ function RecordFacilityDataController($scope, $location, $route, $routeParams, d
     };
 
     if (result) {
+      var facilityDataIncomplete = _.filter($scope.distribution.facilityDistributions, function (facilityDistribution) {
+        return facilityDistribution.computeStatus(false, true) !== DistributionStatus.COMPLETE;
+      });
+
+      if (facilityDataIncomplete.length) {
+        $scope.errorMessage = messageService.get('msg.sync.error.incomplete.data');
+        return;
+      }
+
       var promises = [];
 
       $.each($scope.distribution.facilityDistributions, function (ignore, facilityDistribution) {
@@ -233,21 +242,13 @@ function RecordFacilityDataController($scope, $location, $route, $routeParams, d
   }
 
   $scope.sync = function () {
-    var facilityDataIncomplete = _.filter($scope.distribution.facilityDistributions, function (facilityDistribution) {
-      return facilityDistribution.computeStatus(false, true) !== DistributionStatus.COMPLETE;
-    });
+    var dialogOpts = {
+      id: 'distributionSync',
+      header: 'label.distribution.sync',
+      body: 'msg.sync.edit.data'
+    };
 
-    if (facilityDataIncomplete.length) {
-      $scope.errorMessage = messageService.get('msg.sync.error.incomplete.data');
-    } else {
-      var dialogOpts = {
-        id: 'distributionSync',
-        header: 'label.distribution.sync',
-        body: 'msg.sync.edit.data'
-      };
-
-      OpenLmisDialog.newDialog(dialogOpts, syncCallback, $dialog);
-    }
+    OpenLmisDialog.newDialog(dialogOpts, syncCallback, $dialog);
   };
 
   $scope.abandonAll = function () {
